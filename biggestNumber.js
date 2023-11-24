c69313bcae46762a57aa6695936044591d4ea550
eddiemuhoro <eddiemuhoro@gmail.com>
2023-11-24 19:48:18 +0300

    (add) interview questions files

---
+++ biggestNumber.js
@@ -0,0 +1,19 @@
+//Find the Largest Number in an Array: 
+//Write a function that takes an array of numbers as input and returns the largest number in the array.
+
+const array = [32,42,54,23,43];
+
+const biggestNo = (array) => {
+    let biggest=0
+    for(let i=0; i<array.length; i++){
+       
+        if(biggest < array[i]){
+            biggest = array[i]
+        }else{
+            biggest = biggest
+        }
+    }
+
+    return biggest;
+}
+console.log(biggestNo(array))
---
+++ evenOddNumbers.js
@@ -0,0 +1,15 @@
+//Check if a Number is Even or Odd: 
+//Write a function that takes a number as input and returns "even"
+// if the number is even, and "odd" if the number is odd.
+
+const number = 2;
+
+const detectNumberType = (number) => {
+    if(number % 2 === 0){
+        return "even"
+    }else{
+        return "Odd"
+    }
+}
+
+console.log(detectNumberType(number))
