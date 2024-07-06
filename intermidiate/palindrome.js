3dcd3fddfbf0c681680c75de57e804ecdf55de75
eddiemuhoro <eddiemuhoro@gmail.com>
2024-07-06 16:27:47 +0300

    Refactor palindrome checker function to use array reverse method

--- intermidiate/palindrome.js
+++ intermidiate/palindrome.js
@@ -2,21 +2,31 @@
 // Write a function that takes a string as input and returns true if the string is a palindrome 
 //(reads the same backward as forward), and false otherwise.
 
-const string = 'hannah'
+const string = 'hannanhhhh'
+
+// const checkPalindrome = (string) => {
+//     let lastToStart = '';
+
+//     for(let i = string.length-1; i >=0; i--){
+//         lastToStart += string[i];
+//     }
+
+//     if (string == lastToStart){
+//         return true;
+//     }else {
+//         return false;
+//     }
+
+// }
+
 
 const checkPalindrome = (string) => {
-    let lastToStart = '';
-
-    for(let i = string.length-1; i >=0; i--){
-        lastToStart += string[i];
+    const array = string.split('').reverse().join('')
+    if (array == string){
+        return "Is palindrome"
+    } else{
+        return "Not palindrome"
     }
-
-    if (string == lastToStart){
-        return true;
-    }else {
-        return false;
-    }
-
-}
+} 
 
 console.log(checkPalindrome(string));
