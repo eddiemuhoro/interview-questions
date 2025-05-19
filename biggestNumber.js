71a68da169f02adda45dbe58302add1216237d36
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-20 02:16:26 +0300

    refactor: rename biggestNo function to largestNo and improve formatting

--- biggestNumber.js
+++ biggestNumber.js
@@ -1,19 +1,24 @@
-//Find the Largest Number in an Array: 
+//Find the Largest Number in an Array:
 //Write a function that takes an array of numbers as input and returns the largest number in the array.
 
-const array = [32,42,54,23,43,56];
-
-const biggestNo = (array) => {
-    let biggest=0;
-    for(let i=0; i<array.length; i++){
-       
-        if(biggest < array[i]){
-            biggest = array[i];
-        }else{
-            biggest = biggest;
-        }
+const array = [32, 42, 54, 23, 43, 56];
+console.log("biggest No");
+const largestNo = (array) => {
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    } else {
+      max = max;
     }
+  }
+  return max;
+};
 
-    return biggest;
+console.log(largestNo(array));
+
+function hey() {
+  return "Hello";
 }
-console.log(biggestNo(array));
+
+hey();
