4098eb5501f02a245dfe976bd635887e6d7e7eb1
eddiemuhoro <eddiemuhoro@gmail.com>
2025-06-25 15:23:51 +0300

    fix: update palindrome check to handle special characters and improve string refinement

--- javascript/string/palindrome.ts
+++ javascript/string/palindrome.ts
@@ -1,8 +1,15 @@
-const paliName: string = "Hannah";
+const paliName: string = "Han n,ah";
 
 const checkPalindrome = (paliName: string): boolean => {
-  const refinedString = paliName.toLowerCase();
-  const initiArray = refinedString.split("").reverse().join("");
+  let refinedString = paliName.toLowerCase();
+  const regex = /[^a-z0-9]/g;
+  refinedString = refinedString.replace(regex, "");
+  console.log(refinedString);
+  const initiArray = refinedString
+    .replace(regex, "")
+    .split("")
+    .reverse()
+    .join("");
   if (refinedString === initiArray) {
     return true;
   }
