58969278c94d9d0a4fd406272350cf48b46c3d5a
eddiemuhoro <eddiemuhoro@gmail.com>
2025-06-27 22:18:57 +0300

    add: initialize secretSanta functionality with player setup and placeholder functions

---
+++ javascript/arrays/secretSanta.js
@@ -0,0 +1,12 @@
+const players = ["Dan", "Ken", "Ann", "Ian"];
+// return [
+//     ['Dan', 'Ian'],['Ken','Dan'],['Ian','Ken']
+// ]
+const players_map = new Set();
+const final = [];
+
+const go = (players) => {
+  return final;
+};
+
+const shuffle = (players) => {};
