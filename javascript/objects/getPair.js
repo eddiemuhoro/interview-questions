b84a0d5b0f3d090be2070d4e4d1c9433306585b5
eddiemuhoro <eddiemuhoro@gmail.com>
2025-07-05 17:15:22 +0300

    style: format code for consistency and readability

--- javascript/objects/getPair.js
+++ javascript/objects/getPair.js
@@ -1,46 +1,45 @@
 const persons = [
-    {
-        id: 1,
-        first: "Edwin",
-        last: "Ken",
-        age: 21,
-        goals: 30
-    },
-    {
-        id: 1,
-        first: "Tony",
-        last: "Muho",
-        age: 27,
-        goals: 30
-    },
-    {
-        id: 1,
-        first: "Tabby",
-        last: "Wan",
-        age: 21,
-        goals: 30
-    },
-]
+  {
+    id: 1,
+    first: "Edwin",
+    last: "Ken",
+    age: 21,
+    goals: 30,
+  },
+  {
+    id: 1,
+    first: "Tony",
+    last: "Muho",
+    age: 27,
+    goals: 30,
+  },
+  {
+    id: 1,
+    first: "Tabby",
+    last: "Wan",
+    age: 21,
+    goals: 30,
+  },
+];
 
 //object by first name
-const statsByName = persons.reduce((acc, curr,index, array) => {
+const statsByName = persons.reduce((acc, curr, index, array) => {
+  acc[curr.first] = curr;
 
-        acc[curr.first] = {curr}
+  return acc;
+}, []);
 
-    return acc
-}, {})
-
-console.log(statsByName)
+console.log(statsByName);
 
 const initials = persons.reduce((acc, curr, index, array) => {
-    if(index === array.length){
-        return;
-    }else{
-        const split = curr.first[0]
-        acc.push(`${curr.first[0]}${curr.last[0]}.`)
-    }
-    return acc
-}, [])
+  if (index === array.length) {
+    return;
+  } else {
+    const split = curr.first[0];
+    acc.push(`${curr.first[0]}${curr.last[0]}.`);
+  }
+  return acc;
+}, []);
 
 // console.log(initials);
 
@@ -51,13 +50,13 @@ const result = persons.map((person) => person.first);
 
 //no of same age [23: 2, 33:1]
 const ageCount = persons.reduce((acc, curr) => {
-   if(acc[curr.age]) {
-        acc[curr.age]++
-   }else{
-    acc[curr.age] = 1
-   }
+  if (acc[curr.age]) {
+    acc[curr.age]++;
+  } else {
+    acc[curr.age] = 1;
+  }
 
-   return acc
-}, {})
+  return acc;
+}, {});
 
 // console.log(ageCount);
