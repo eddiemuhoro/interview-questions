d659fc3a908f7a9187808968548070364f402236
eddiemuhoro <eddiemuhoro@gmail.com>
2025-07-06 23:14:06 +0300

    add: implement object creation and manipulation with dynamic properties

---
+++ javascript/objects/objectCreation.js
@@ -0,0 +1,25 @@
+//define properties using strings
+const person = {
+  "first name": "Edwin",
+  "last name": "Ken",
+  age: 21,
+  goals: 30,
+};
+
+person["full name with age"] = function () {
+  return `${this["first name"]} ${this["last name"]} is ${this.age} years old`;
+};
+person.yob = 2000;
+person["first name"] = "Jan";
+
+Object.assign(person, {
+  "last name": "Doe",
+  age: 22,
+});
+console.log(person, person["full name with age"]()); // Accessing property with space in the name
+
+const updatedObj = Object.assign({}, person, {
+  "first name": "John",
+});
+
+console.log(updatedObj["full name with age"]()); // { first name: 'John' }
