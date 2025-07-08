43a0d3e92b3ecb358be07ae8dd8c952efa126194
eddiemuhoro <eddiemuhoro@gmail.com>
2025-07-08 23:10:25 +0300

    refactor: optimize getNums function using a hashmap for improved performance

--- javascript/arrays/twoSum.js
+++ javascript/arrays/twoSum.js
@@ -7,18 +7,26 @@ let output = [];
 const target = 6;
 
 function getNums(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (i !== j) {
-        let result = nums[i] + nums[j];
-        console.log(result);
-        if (target === result) {
-          output.push(i, j);
-          return output;
-        }
-      }
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = 0; j < nums.length; j++) {
+  //     if (i !== j) {
+  //       let result = nums[i] + nums[j];
+  //       console.log(result);
+  //       if (target === result) {
+  //         output.push(i, j);
+  //         return output;
+  //       }
+  //     }
+  //   }
+  // }
+  const numsMap = new Map();
+  for (let j = 0; j < nums.length; j++) {
+    const complement = target - nums[j];
+    if (numsMap.has(complement)) {
+      return [numsMap.get(complement), j];
     }
+    numsMap.set(nums[j], j);
   }
+  return [];
 }
-
 console.log(getNums(nums, target));
